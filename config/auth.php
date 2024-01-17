eb3953ade96f87e6af93f8d4c785890a2b37c7cf
mr-lunart <dewanata_85@yahoo.com>
2024-01-17 13:45:22 +0700

    deleted verifiy login midleware

--- config/auth.php
+++ config/auth.php
@@ -92,7 +92,7 @@ return [
 
     'passwords' => [
         'users' => [
-            'provider' => 'users',
+            'provider' => 'admins',
             'table' => 'password_reset_tokens',
             'expire' => 60,
             'throttle' => 60,
