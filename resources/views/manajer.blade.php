a18fbb5d826725bae18cbe4d3801aa20c637214f
mr-lunart <dewanata_85@yahoo.com>
2024-01-17 22:36:27 +0700

    add table list, edit, delete and view button

--- resources/views/manajer.blade.php
+++ resources/views/manajer.blade.php
@@ -5,9 +5,9 @@
 @endsection
 
 @section('content')
-<div class="container border px-4 py-5 font-sans">
+<div class="container border px-4 py-5 font-sans rounded-md">
     <span class="">
-        <h1 class="text-2xl font-semibold">Administrator</h1>
+        <h1 class="text-xl font-semibold"><bold>Manajer Admin</blod></h1>
     </span>
 </div>
 <br>
@@ -15,26 +15,34 @@
     <table class="table-auto w-full">
         <thead class="">
             <tr>
-            <th class="border-b pt-0 pb-3 text-left">Song</th>
-            <th class="border-b pt-0 pb-3 text-left">Artist</th>
-            <th class="border-b pt-0 pb-3 text-left">Year</th>
+            <th class="border-b pt-0 pb-3 text-left">Nama Admin</th>
+            <th class="border-b pt-0 pb-3 text-left">Tanggal Dibuat</th>
+            <th class="border-b pt-0 pb-3 text-left">Hak Akses</th>
+            <th class="border-b pt-0 pb-3 text-left">Opsi</th>
             </tr>
         </thead>
         <tbody class="text-sm">
-            <tr>
-            <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-            <td>Malcolm Lockyer</td>
-            <td>1961</td>
+            <tr class="border">
+                <td class="p-3">The Sliding Mr. Bones </td>
+                <td></td>
+                <td class="p-3">
+                    <button class="flex justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-500">Lihat Hak</button>
+                </td>
+                <td class="p-3">
+                    <button class="rounded-md bg-red-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-500">Hapus</button>
+                    <button class="rounded-md bg-blue-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500">Edit</button>
+                </td>
             </tr>
-            <tr>
-            <td>Witchy Woman</td>
-            <td>The Eagles</td>
-            <td>1972</td>
-            </tr>
-            <tr>
-            <td>Shining Star</td>
-            <td>Earth, Wind, and Fire</td>
-            <td>1975</td>
+            <tr class="border">
+                <td class="p-3">The Sliding Mr. Bones </td>
+                <td></td>
+                <td class="p-3">
+                    <button class="flex justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-500">Lihat Hak</button>
+                </td>
+                <td class="p-3">
+                    <button class="rounded-md bg-red-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-500">Hapus</button>
+                    <button class="rounded-md bg-blue-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500">Edit</button>
+                </td>
             </tr>
         </tbody>
     </table>
