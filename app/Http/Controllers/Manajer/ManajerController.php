67bff4a2e7821bbe5db64a47cb0190862fca1a46
mr-lunart <dewanata_85@yahoo.com>
2024-01-19 13:45:51 +0700

    add insert admin method on manajer controller

--- app/Http/Controllers/Manajer/ManajerController.php
+++ app/Http/Controllers/Manajer/ManajerController.php
@@ -1,14 +1,24 @@
 <?php
 
 namespace App\Http\Controllers\Manajer;
-
-use App\Models\Admin;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Admin;
 
 class ManajerController extends Controller
 {
-    public static function getAdmins() {
+    public static function getAdmin() {
         $admins = Admin::all();
         return $admins;
+    }
+    public static function createAdmin(Request $request) {
+        
+        $query = Admin::factory()->create([
+            'name' => $request->input('name'),
+            'username' => $request->input('username'),
+            'password' => $request->input('password'),
+            'roles'=>'administrator'
+        ]);
+        return $query;
     }
 }
