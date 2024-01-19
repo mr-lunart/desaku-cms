302741bc640ded8d0718ab14ce1a45aeb9959838
mr-lunart <dewanata_85@yahoo.com>
2024-01-20 00:23:47 +0700

    add exception handler for insert data to new admin

--- app/Http/Controllers/Manajer/ManajerController.php
+++ app/Http/Controllers/Manajer/ManajerController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Manajer;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Admin;
+use Exception;
+use Illuminate\Support\Facades\Hash;
 
 class ManajerController extends Controller
 {
@@ -13,12 +15,27 @@ class ManajerController extends Controller
     }
     public static function createAdmin(Request $request) {
         
-        $query = Admin::factory()->create([
-            'name' => $request->input('name'),
-            'username' => $request->input('username'),
-            'password' => $request->input('password'),
-            'roles'=>'administrator'
-        ]);
-        return $query;
+        try {
+            $query = Admin::factory()->create([
+                'name' => $request->input('name'),
+                'username' => $request->input('username'),
+                'password' => Hash::make($request->input('password')),
+                'roles'=>'administrator'
+            ]);
+
+            return redirect()->route('manajer',['status'=>'failed','admin_no'=>$query->no,'username'=>$query->username]);
+
+        } catch (Exception $e) {
+            $except= explode(':',$e->getMessage())[0];
+            if($except == 'SQLSTATE[23000]'){
+                return redirect()->route('manajer',['status'=>'failed']);
+            }
+            else{
+                return redirect()->route('manajer',['status'=>'uknown error']);
+            }
+            
+        }
+        
+        
     }
 }
--- resources/views/manajerAddAdmin.blade.php
+++ resources/views/manajerAddAdmin.blade.php
@@ -6,39 +6,39 @@
 
 @section('content')
 <div class="container">
-<form>
+<form id="" action="{{route('manajerCreate')}}" method="POST">
+@csrf
   <div class="space-y-12">
-
     <div class="border-b border-gray-900/10 pb-12">
-      <h2 class="text-base font-semibold leading-7 text-gray-900">Personal Information</h2>
+      <h2 class="text-base font-semibold leading-7 text-gray-900">Form Admin Baru</h2>
       <p class="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p>
 
       <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
         <div class="sm:col-span-3">
           <label for="first-name" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
           <div class="mt-2">
-            <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+            <input type="text" name="username" id="username" autocomplete="given-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
           </div>
         </div>
 
         <div class="sm:col-span-3">
           <label for="last-name" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
           <div class="mt-2">
-            <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+            <input type="password" name="password" id="password" autocomplete="family-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
           </div>
         </div>
 
         <div class="sm:col-span-3">
           <label for="first-name" class="block text-sm font-medium leading-6 text-gray-900">Nama Lengkap</label>
           <div class="mt-2">
-            <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+            <input type="text" name="name" id="name" autocomplete="given-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
           </div>
         </div>
 
         <div class="sm:col-span-3">
           <label for="last-name" class="block text-sm font-medium leading-6 text-gray-900">Email</label>
           <div class="mt-2">
-            <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+            <input type="email" name="email" id="email" autocomplete="family-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
           </div>
         </div>
 
@@ -57,7 +57,7 @@
 
   <div class="mt-6 flex items-center justify-end gap-x-6">
     <a href="{{route('manajer')}}"><button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button></a>
-    <a href="{{route('manajerCreate')}}"></a><button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+    <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
   </div>
 </form>
 </div>
