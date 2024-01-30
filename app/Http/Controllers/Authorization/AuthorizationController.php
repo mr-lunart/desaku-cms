fcf06fab9ad111696a099a033099fe48c5747cfd
mr-lunart <dewanata_85@yahoo.com>
2024-01-30 23:14:30 +0700

    add getPermit method, AuthorizationCtrl

--- app/Http/Controllers/Authorization/AuthorizationController.php
+++ app/Http/Controllers/Authorization/AuthorizationController.php
@@ -9,7 +9,18 @@ class AuthorizationController extends Controller
 {
     public function __construct()
     {
-        
+    }
+
+    public function getPermmision($permission)
+    {
+        $action = $permission;
+        if(function_exists($action)){
+            //call the method and save the value
+            return $this->$action();
+        }
+        else {
+            return ['method'=>404];
+        }
     }
 
     public static function showDashboard()
